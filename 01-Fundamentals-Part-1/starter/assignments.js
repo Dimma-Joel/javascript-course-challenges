04dc91a31273decf2f28f3e0bbb25e1639a784ce
DIMMAS-LAPTOP\Dimma Joel <dimmajoel@gmail.com>
2025-01-15 16:01:02 +0100

    Added values and variables assignment instruction

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,3 +1,7 @@
+// 1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+//2. Log their values to the console.
+
 let country = "Nigeria";
 let continent = "Africa";
 let population = 100;
