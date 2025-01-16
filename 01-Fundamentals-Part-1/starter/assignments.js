25fdf42a2db66a834ee275625777b0c17157ee9b
DIMMAS-LAPTOP\Dimma Joel <dimmajoel@gmail.com>
2025-01-16 17:39:50 +0100

    Worked on data types assignment

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,3 +1,5 @@
+//Assignment 1
+
 // 1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
 
 //2. Log their values to the console.
@@ -9,3 +11,12 @@ let population = 100;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+//Assignment 2
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
