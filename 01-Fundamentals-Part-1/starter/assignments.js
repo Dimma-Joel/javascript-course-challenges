ab71dba025c3feb7b8acae8b1676300e7d3ccb6a
DIMMAS-LAPTOP\Dimma Joel <dimmajoel@gmail.com>
2025-01-17 22:27:25 +0100

    If/else statement challenge

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,43 +1,3 @@
-//Values and variables
-
-// 1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
-
-//2. Log their values to the console.
-
-const country = "Nigeria";
-const continent = "Africa";
-let population = 10;
-
-console.log(country);
-console.log(continent);
-console.log(population);
-
-//Data Types
-
-const isIsland = false;
-let language = "English";
-
-// isIsland = true;
-
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
-
-//Basic operators
-console.log(population / 2);
-
-population++;
-console.log(population);
-
-let finlandPopulation = 6;
-let comparePopulation = population > finlandPopulation;
-console.log(comparePopulation);
-
-let averagePopulation = 33;
-let compareAveragePopulation = population < averagePopulation;
-console.log(compareAveragePopulation);
-
 //CHALLENGE #1
 
 /*Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height) (mass in kg and height in meters).
@@ -113,6 +73,57 @@ IMPORTANT: The ** operator is not supported in this editor. Please make sure to 
 //   " million people speak " +
 //   language;
 
-const description = `${country} is in ${continent}, and its ${population} million people speal ${language}`.;
+///////////////////////////
+//Values and variables
+
+// 1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+//2. Log their values to the console.
+
+const country = "Nigeria";
+const continent = "Africa";
+let population = 10;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//Data Types
+
+const isIsland = false;
+let language = "English";
+
+// isIsland = true;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+//Basic operators
+console.log(population / 2);
+
+population++;
+console.log(population);
+
+let finlandPopulation = 6;
+let comparePopulation = population > finlandPopulation;
+console.log(comparePopulation);
+
+let averagePopulation = 33;
+let compareAveragePopulation = population < averagePopulation;
+console.log(compareAveragePopulation);
+
+const description = `${country} is in ${continent}, and its ${population} million people speal ${language}.`;
 
 console.log(description);
+
+////////////////////////////
+// Challenge 2
+
+if (averagePopulation > population) {
+  const average = averagePopulation - population;
+  console.log(`${country}'s population is ${average} million below average`);
+} else {
+  console.log(`${country}'s population is above average`);
+}
