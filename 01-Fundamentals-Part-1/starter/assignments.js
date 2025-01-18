2ce6d8cf19836ca6484e6bb1c450e36f179af442
DIMMAS-LAPTOP\Dimma Joel <dimmajoel@gmail.com>
2025-01-19 00:18:41 +0100

    Conquered the third challenge

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,3 +1,6 @@
+//CHALLENGES
+
+//////////////////////////////////////////
 //CHALLENGE #1
 
 /*Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height) (mass in kg and height in meters).
@@ -62,6 +65,49 @@ IMPORTANT: The ** operator is not supported in this editor. Please make sure to 
 // console.log(BMIJohn);
 // console.log(markHigherBMI);
 
+////////////////////////////////////
+// CHALLENGE 2
+
+if (averagePopulation > population) {
+  const average = averagePopulation - population;
+  console.log(`${country}'s population is ${average} million below average`);
+} else {
+  console.log(`${country}'s population is above average`);
+}
+
+///////////////////////////////////////
+//CHALLENGE #3
+// There are two gymnastics teams: Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+
+// Your tasks:
+
+// 1. Calculate the average score for each team, using the test data included below. The average score for Dolphins should be assigned to the scoreDolphins variable, and the average score of Koalas should be assigned to the scoreKoalas variable.
+
+// 2. Compare the team's average scores to determine the winner of the competition, and print to the console:
+
+// "Dolphins win the trophy" if Dolphins win, or
+
+// "Koalas win the trophy" if Koalas win, or
+
+// "Both win the trophy" if their average scores are equal.
+
+// TEST DATA: Dolphins scored 96, 108, and 89. Koalas scored 88, 91, and 110.
+
+const scoreDolphins = 96 + 108 + 89 / 2;
+const scoreKoalas = 88 + 91 + 110 / 2;
+
+console.log(scoreDolphins);
+console.log(scoreKoalas);
+
+if (scoreDolphins > scoreKoalas) {
+  console.log("Dolphins win the trophy");
+} else if (scoreKoalas > scoreDolphins) {
+  console.log("Koalas win the trophy");
+} else if (scoreDolphins === scoreKoalas) {
+  console.log("Both win the trophy");
+}
+
+//ASSIGNMENTS
 ///////////////////////////////
 //Strings and template literals
 // const description =
@@ -117,16 +163,6 @@ console.log(compareAveragePopulation);
 const description = `${country} is in ${continent}, and its ${population} million people speal ${language}.`;
 
 console.log(description);
-
-////////////////////////////
-// Challenge 2
-
-if (averagePopulation > population) {
-  const average = averagePopulation - population;
-  console.log(`${country}'s population is ${average} million below average`);
-} else {
-  console.log(`${country}'s population is above average`);
-}
 
 //Type coercion and conversion
 //Predict the result of these 5 operations without executing them:
