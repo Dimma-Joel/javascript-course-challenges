f3afe942cc5b333d3b64c8f5251caf2d1177cc8f
DIMMAS-LAPTOP\Dimma Joel <dimmajoel@gmail.com>
2025-01-18 19:16:58 +0100

    Type coercion and conversion

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -127,3 +127,13 @@ if (averagePopulation > population) {
 } else {
   console.log(`${country}'s population is above average`);
 }
+
+//Type coercion and conversion
+
+//Predict the result of these 5 operations without executing them:
+
+console.log("9" - "5"); // -> ? 4
+console.log("19" - "13" + "17"); // -> ? '617'
+console.log("19" - "13" + 17); // -> ? 23
+console.log("123" < 57); // -> ? false
+console.log(5 + 6 + "4" + 9 - 4 - 2); // -> ? 1143
